3abac40529a77d78c0e28cca4ad1ac5bb9db7a19
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-01-30 20:00:28 +0300

    init1

--- src/main/java/com/example/dadata/Controller/DadataController.java
+++ src/main/java/com/example/dadata/Controller/DadataController.java
@@ -1,11 +1,9 @@
 package com.example.dadata.Controller;
 
-import com.example.dadata.domain.Suggestion;
+import com.example.dadata.domain.Dadata;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +15,7 @@ public class DadataController {
 
 
     @PostMapping("/")
-    public Suggestion getMovieByTitle() throws JsonProcessingException {
+    public Dadata getMovieByTitle() throws JsonProcessingException {
 
         String accessToken = "3690dff6fc202c750c97d780fe333d2c063dadcd";
         final String url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
@@ -27,11 +25,14 @@ public class DadataController {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set(HttpHeaders.AUTHORIZATION, "Token " + accessToken);
 
-        String requestJson = "{\"query\":\"казань\"}";
+        String requestJson = "{\"query\":\"Татарстан город казань\"}";
         HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
         ObjectMapper objectMapper = new ObjectMapper();
 
-        return restTemplate.postForObject(url, entity, Suggestion.class);
+
+        Dadata dadata = restTemplate.postForObject(url, entity, Dadata.class);
+
+
 
 //        DadataRequest dadataRequest = objectMapper.readValue(str, DadataRequest.class);
     }
