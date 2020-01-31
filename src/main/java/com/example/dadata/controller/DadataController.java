6f77619941876aeccdff3f214cf36e551d44b0f0
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-01-31 10:42:04 +0300

    init

---
+++ src/main/java/com/example/dadata/controller/DadataController.java
@@ -0,0 +1,28 @@
+package com.example.dadata.controller;
+
+import com.example.dadata.API.DadataApi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class DadataController {
+
+    @GetMapping("/{location}")
+    public Map<String, String> getLocation(@PathVariable("location") String location) {
+        DadataApi api = new DadataApi();
+
+        Map<String, String> map = new HashMap<>();
+
+        api.DadataClient(location)
+                .getSuggestions()
+                .forEach(suggestions -> map.put(suggestions.getValue() + " Код КЛАДР региона ",
+                        suggestions.getData().getRegionKladrId()));
+        return map;
+    }
+}
--- src/main/java/com/example/dadata/domain/Dadata.java
+++ src/main/java/com/example/dadata/domain/Dadata.java
@@ -2,86 +2,173 @@ package com.example.dadata.domain;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
-/**
- * @see <a href="https://confluence.hflabs.ru/pages/viewpage.action?pageId=204669103">Dadata address object</a>
- */
+import java.util.Set;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Dadata {
+    private Set<Suggestions> suggestions;
 
-    private Suggestions[] suggestions;
 
-    public Dadata() {
-    }
-
-    public Suggestions[] getSuggestions() {
-        return suggestions;
-    }
-
-    public void setSuggestions(Suggestions[] suggestions) {
-        this.suggestions = suggestions;
-    }
-
-    public static class Metro {
-
-    }
-
+    @lombok.Data
     public static class Data {
 
-        @JsonAlias("country")
+        @JsonAlias("postal_code")
+        private String postalCode;
         private String country;
+        @JsonAlias("country_iso_code")
+        private String countryIsoCode;
+        @JsonAlias("federal_district")
+        private String federalDistrict;
+        @JsonAlias("region_fias_id")
+        private String regionFiasId;
         @JsonAlias("region_kladr_id")
         private String regionKladrId;
-
-        public String getRegionKladrId() {
-            return regionKladrId;
-        }
-
-        public void setRegionKladrId(String regionKladrId) {
-            this.regionKladrId = regionKladrId;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
+        @JsonAlias("region_iso_code")
+        private String regionIsoCode;
+        @JsonAlias("region_with_type")
+        private String regionWithType;
+        @JsonAlias("region_type")
+        private String regionType;
+        @JsonAlias("region_type_full")
+        private String regionTypeFull;
+        private String region;
+        @JsonAlias("area_fias_id")
+        private String areaFiasId;
+        @JsonAlias("area_kladr_id")
+        private String areaKladrId;
+        @JsonAlias("area_with_type")
+        private String areaWithType;
+        @JsonAlias("area_type")
+        private String areaType;
+        @JsonAlias("area_type_full")
+        private String areaTypeFull;
+        private String area;
+        @JsonAlias("city_fias_id")
+        private String cityFiasId;
+        @JsonAlias("city_kladr_id")
+        private String cityKladrId;
+        @JsonAlias("city_with_type")
+        private String cityWithType;
+        @JsonAlias("city_type")
+        private String cityType;
+        @JsonAlias("city_type_full")
+        private String cityTypeFull;
+        private String city;
+        @JsonAlias("city_area")
+        private String cityArea;
+        @JsonAlias("city_district_fias_id")
+        private String cityDistrictFiasId;
+        @JsonAlias("city_district_kladr_id")
+        private String cityDistrictKladrId;
+        @JsonAlias("city_district_with_type")
+        private String cityDistrictWithType;
+        @JsonAlias("city_district_type")
+        private String cityDistrictType;
+        @JsonAlias("city_district_type_full")
+        private String cityDistrictTypeFull;
+        @JsonAlias("city_district")
+        private String cityDistrict;
+        @JsonAlias("settlement_fias_id")
+        private String settlementFiasId;
+        @JsonAlias("settlement_kladr_id")
+        private String settlementKladrId;
+        @JsonAlias("settlement_with_type")
+        private String settlementWithType;
+        @JsonAlias("settlement_type")
+        private String settlementType;
+        @JsonAlias("settlement_type_full")
+        private String settlementTypeFull;
+        private String settlement;
+        @JsonAlias("street_fias_id")
+        private String streetFiasId;
+        @JsonAlias("street_kladr_id")
+        private String streetKladrId;
+        @JsonAlias("street_with_type")
+        private String streetWithType;
+        @JsonAlias("street_type")
+        private String streetType;
+        @JsonAlias("street_type_full")
+        private String streetTypeFull;
+        private String street;
+        @JsonAlias("house_fias_id")
+        private String houseFiasId;
+        @JsonAlias("house_kladr_id")
+        private String houseKladrId;
+        @JsonAlias("house_type")
+        private String houseType;
+        @JsonAlias("house_type_full")
+        private String houseTypeFull;
+        private String house;
+        @JsonAlias("block_type")
+        private String blockType;
+        @JsonAlias("block_type_full")
+        private String blockTypeFull;
+        private String block;
+        @JsonAlias("flat_type")
+        private String flatType;
+        @JsonAlias("flat_type_full")
+        private String flatTypeFull;
+        private String flat;
+        @JsonAlias("flat_area")
+        private Double flatArea;
+        @JsonAlias("square_meter_price")
+        private Double squareMeterPrice;
+        @JsonAlias("flat_price")
+        private Double flatPrice;
+        @JsonAlias("postal_box")
+        private String postalBox;
+        @JsonAlias("fias_id")
+        private String fiasId;
+        @JsonAlias("fias_code")
+        private String fiasCode;
+        @JsonAlias("fias_level")
+        private String fiasLevel;
+        @JsonAlias("fias_actuality_state")
+        private String fiasActualityState;
+        @JsonAlias("kladr_id")
+        private String kladrId;
+        @JsonAlias("geoname_id")
+        private String geonameId;
+        @JsonAlias("capital_marker")
+        private String capitalMarker;
+        private String okato;
+        private String oktmo;
+        @JsonAlias("tax_office")
+        private String taxOffice;
+        @JsonAlias("tax_office_legal")
+        private String taxOfficeLegal;
+        @JsonAlias("geo_lat")
+        private Double geoLat;
+        @JsonAlias("geo_lon")
+        private Double geoLon;
+        @JsonAlias("beltway_hit")
+        private String beltwayHit;
+        @JsonAlias("beltway_distance")
+        private Double beltwayDistance;
+        @JsonAlias("qc_geo")
+        private String qcGeo;
+        @JsonAlias("qc_complete")
+        private String qcComplete;
+        @JsonAlias("qc_house")
+        private String qcHouse;
+        @JsonAlias("unparsed_parts")
+        private String unparsedParts;
+        private String source;
+        private String qc;
     }
 
+    @lombok.Data
     public static class Suggestions {
         private String value;
 
         @JsonAlias("unrestricted_value")
         private String unrestrictedValue;
-
         private Data data;
 
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-
-        public String getUnrestrictedValue() {
-            return unrestrictedValue;
-        }
-
-        public void setUnrestrictedValue(String unrestrictedValue) {
-            this.unrestrictedValue = unrestrictedValue;
-        }
-
-        public Data getData() {
-            return data;
-        }
-
-        public void setData(Data data) {
-            this.data = data;
-        }
     }
 
 }
+
